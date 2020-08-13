13d84ab77feb6ae19cf26dd24069dc42186e2975
Andrew Andersen <andersac88@gmail.com>
2020-08-12 21:06:47 -0500

    routes

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate'
+import StreamDelete from './streams/StreamDelete'
+import StreamEdit from './streams/StreamEdit'
+import StreamShow from './streams/StreamShow'
+import StreamList from './streams/StreamList'
 
 const App = () => {
 	return (
-    <div>App</div>
+    <div>
+        <BrowserRouter>
+        <div>
+            <Route path='/' exact component={StreamList} />
+            <Route path='/streams/new' component={StreamCreate} />
+            <Route path='/streams/edit' component={StreamEdit} />
+            <Route path='/streams/delete' component={StreamDelete} />
+            <Route path='/streams/show' component={StreamShow} />
+        </div>
+        </BrowserRouter>
+    </div>
     )
 };
 
