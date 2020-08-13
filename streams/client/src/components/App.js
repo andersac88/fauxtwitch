3e5ab904451f40a2e710ee260edfd8cbb7aebce4
Andrew Andersen <andersac88@gmail.com>
2020-08-12 21:20:13 -0500

    router with header

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
-import StreamCreate from './streams/StreamCreate'
-import StreamDelete from './streams/StreamDelete'
-import StreamEdit from './streams/StreamEdit'
-import StreamShow from './streams/StreamShow'
-import StreamList from './streams/StreamList'
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamShow from './streams/StreamShow';
+import StreamList from './streams/StreamList';
+import Header from './Header';
 
 const App = () => {
 	return (
-    <div>
-        <BrowserRouter>
-        <div>
-            <Route path='/' exact component={StreamList} />
-            <Route path='/streams/new' component={StreamCreate} />
-            <Route path='/streams/edit' component={StreamEdit} />
-            <Route path='/streams/delete' component={StreamDelete} />
-            <Route path='/streams/show' component={StreamShow} />
-        </div>
-        </BrowserRouter>
-    </div>
-    )
+		<div className="ui container">
+			<BrowserRouter>
+				<Header />
+				<div>
+					<Route path="/" exact component={StreamList} />
+					<Route path="/streams/new" component={StreamCreate} />
+					<Route path="/streams/edit" component={StreamEdit} />
+					<Route path="/streams/delete" component={StreamDelete} />
+					<Route path="/streams/show" component={StreamShow} />
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
