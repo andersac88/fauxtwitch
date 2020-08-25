82f5b7b85f7ba8da711603940e797d36d4568c29
Andrew Andersen <andersac88@gmail.com>
2020-08-24 20:57:35 -0500

    update

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -12,7 +12,9 @@ class StreamList extends React.Component {
 		if (stream.userId === this.props.currentUserId) {
 			return (
 				<div className="right floated content">
-					<button className="ui button primary">Edit</button>
+					<Link className="ui button primary" to={`/streams/edit/${stream.id}`}>
+						Edit
+					</Link>
 					<button className="ui button negative">Delete</button>
 				</div>
 			);
@@ -23,7 +25,7 @@ class StreamList extends React.Component {
 		return this.props.streams.map((stream) => {
 			return (
 				<div className="item" key={stream.id}>
-					{this.renderAdmin(stream)}
+					{this.renderAdmin(stream)} 
 					<i className="large middle aligned icon camera" />
 					<div className="content">
 						{stream.title}
